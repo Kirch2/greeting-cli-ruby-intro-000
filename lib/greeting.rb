4fd3db1048234082a756bfd8d9f6e7d0aa371810
Chris Kirchner <kirch2@gmail.com>
2021-06-22 20:55:53 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,7 @@
 # code the #greeting method here!
 puts "Please enter your name so that we can greet you more personally:"
 name = gets.strip
+greeting(name)
  def greeting(name)
   puts "Hello #{name}. It's nice to meet you."
 end
